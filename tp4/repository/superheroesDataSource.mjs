0b9925af903891ac16ae7124fc68a89df3e75219
pablosegundo31 <pablosegundolobos18@gmail.com>
2024-11-01 23:33:19 -0300

    carga

---
+++ tp4/repository/superheroesDataSource.mjs
@@ -0,0 +1,6 @@
+export default class SuperheroresDataSource {
+    obtenerTodos(){
+        throw new Error("Este metodo debe ser implementado por la sub clase");
+        
+    }
+}
---
+++ tp4/services/superheroesService.mjs
@@ -0,0 +1,19 @@
+import SuperheroesRepository from "../repository/superheroesRepository.mjs";
+
+const repository = new SuperheroesRepository();
+
+export function obtenerSuperheroePorId(id) {
+    const superheroes = repository.obtenerTodos();
+    return superheroes.find(hero => hero.id === id);    
+}
+
+export function buscarSuperheroesPorAtributo(atributo, valor) {
+    const superheroes = repository.obtenerTodos();
+    return superheroes.find(hero => hero.id === id);
+}
+
+export function obtenerSuperheroesMayoresDe30 () {
+    const superheroes = repository.obtenerTodos();
+    return superheroes.filter(hero => hero.edad > 30 && hero.planetaOrigen === 'Tierra' && hero.poder.length >= 2);
+    
+}
