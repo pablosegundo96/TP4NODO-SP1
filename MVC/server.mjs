58581be691f5d312f7364b424598622adb24aa4b
pablosegundo31 <pablosegundolobos18@gmail.com>
2024-11-06 21:58:33 -0300

    carga

--- MVC/server.mjs
+++ MVC/server.mjs
@@ -8,9 +8,13 @@ const app = express();
 const PORT = 3005;
 
 //rutas y pruebas comentadas
-app.get('/superheroes/id/:id', obtenerSuperheroePorIdController); //http://localhost:3005/superheroes/id/2
-app.get('/superheroes/atributo/:atributo/:valor', buscarSuperheroesPorAtributoController);//http://localhost:3005/superheroes/atributo/nombreSuperheroe/Superman
-app.get('/superheroes/edad/mayorA30', obtenerSuperheroesMayoresDe30Controller);//http://localhost:3005/superheroes/edad/mayorA30
+app.get('/superheroes/id/:id', obtenerSuperheroePorIdController);
+ //http://localhost:3005/superheroes/id/2
+app.get('/superheroes/atributo/:atributo/:valor', buscarSuperheroesPorAtributoController);
+//http://localhost:3005/superheroes/atributo/nombreSuperheroe/Superman
+app.get('/superheroes/edad/mayorA30', obtenerSuperheroesMayoresDe30Controller);
+//http://localhost:3005/superheroes/edad/mayorA30
+
 
 
 
